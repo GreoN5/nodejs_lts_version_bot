b5f6865d99ce5b167d15a6815341d3b0f606321c
Georgi <georgi38@abv.bg>
2023-02-14 12:26:05 +0200

    feat: add support for nvm usage

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import urllib.request
 
 
 def node_version_parser(version: str):
@@ -7,7 +8,40 @@ def node_version_parser(version: str):
 
 
 def get_current_node_version():
-    return subprocess.check_output('node -v').strip().decode('utf-8')
+    output = run_shell_output(['node', '-v'])
+
+    return output.stdout.decode('utf-8')
+
+
+def check_nvm_usage():
+    '''
+        Returns False if there is no error during execution of the command "nvm -v", otherwise True.
+    '''
+
+    output = run_shell_output(['nvm', '-v'])
+
+    # check if we get error when running the command
+    return bool(output.stderr)
+
+
+def run_shell_output(args):
+    return subprocess.run(args, capture_output=True, shell=True)
+
+
+def run_node_installer(node_version, download_link):
+    current_path = get_current_path()
+    file_extension = '.msi'
+    file_appendix = 'node'
+    file_windows_version = 'x64'
+
+    filename = f'{file_appendix}-{node_version}-{file_windows_version}{file_extension}'
+    url = f'{download_link}{filename}'
+
+    urllib.request.urlretrieve(url, filename)
+    node_exe_path = f'{current_path}\{filename}'
+
+    # run the node installer
+    os.system(node_exe_path)
 
 
 def get_current_path():
